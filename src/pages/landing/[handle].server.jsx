ddcc5ea75220143a5846c748e04fd04bd4ebe1a0
Steve Sewell <steve@builder.io>
2022-02-08 14:41:33 -0800

    Reproduction

---
+++ src/pages/landing/[handle].server.jsx
@@ -0,0 +1,25 @@
+import {useQuery, useUrl} from '@shopify/hydrogen';
+import {builder, BuilderComponent} from '@builder.io/react';
+
+builder.init('cda38653c81344cf8859bd15e4d8e30d');
+
+export default function Page() {
+  // TODO: not working, says not a function
+  // const url = useUrl();
+
+  const content = useQuery('landing-page', async () => {
+    return await builder
+      .get('page', {
+        userAttributes: {
+          urlPath: '/landing/foo',
+        },
+      })
+      .promise();
+  });
+
+  return !content ? (
+    'Loading...'
+  ) : (
+    <BuilderComponent model="page" content={content} />
+  );
+}
