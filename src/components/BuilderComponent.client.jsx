a1957238fad277e2006bb8cc6396eeadd94338df
Steve Sewell <steve@builder.io>
2022-02-09 06:38:41 -0800

    Editor working

---
+++ src/components/BuilderComponent.client.jsx
@@ -0,0 +1,10 @@
+import {BuilderComponent, builder, Builder} from '@builder.io/react';
+// import Welcome from './Welcome.server';
+
+builder.init('cda38653c81344cf8859bd15e4d8e30d');
+
+// Builder.registerComponent(Welcome, {
+//   name: 'Welcome',
+// });
+
+export default BuilderComponent;
--- src/pages/landing/[handle].server.jsx
+++ src/pages/landing/[handle].server.jsx
@@ -1,26 +1,26 @@
-import {useQuery, useUrl} from '@shopify/hydrogen';
-import {builder, BuilderComponent} from '@builder.io/react';
+import {useQuery} from '@shopify/hydrogen';
+import {builder} from '@builder.io/react';
+import BuilderComponent from '../../components/BuilderComponent.client';
+import Layout from '../../components/Layout.server';
 
-builder.init('cda38653c81344cf8859bd15e4d8e30d');
-
-export default function Page() {
-  // TODO: not working, says not a function
-  // const url = useUrl();
-
+export default function Page(props) {
   const content = useQuery('landing-page', async () => {
     return await builder
       .get('page', {
         userAttributes: {
-          urlPath:
-            '/landing/foo' /* TODO: when working, this should be url.pathname */,
+          urlPath: props.pathname,
         },
       })
       .promise();
   });
 
-  return !content ? (
-    'Loading...'
-  ) : (
-    <BuilderComponent model="page" content={content} />
+  return (
+    <Layout>
+      {!content ? (
+        'Loading...'
+      ) : (
+        <BuilderComponent model="page" content={content} />
+      )}
+    </Layout>
   );
 }
