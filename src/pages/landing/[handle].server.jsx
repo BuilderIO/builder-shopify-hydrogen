4512c4192b23ad2bffb8236082e0d7c14941ddaa
Steve Sewell <steve@builder.io>
2022-02-13 09:23:39 -0800

    Improve edit/preview/404 logic

--- src/pages/landing/[handle].server.jsx
+++ src/pages/landing/[handle].server.jsx
@@ -2,11 +2,14 @@ import {useQuery} from '@shopify/hydrogen';
 import {Builder, builder} from '@builder.io/react';
 import BuilderComponent from '../../components/BuilderComponent.client';
 import Layout from '../../components/Layout.server';
+import NotFound from '../../components/NotFound.server';
+
+const MODEL_NAME = 'page';
 
 export default function Page(props) {
-  const content = useQuery('page', async () => {
+  const content = useQuery([MODEL_NAME, props.pathname], async () => {
     return await builder
-      .get('page', {
+      .get(MODEL_NAME, {
         userAttributes: {
           urlPath: props.pathname,
         },
@@ -14,9 +17,16 @@ export default function Page(props) {
       .promise();
   });
 
+  const params = new URLSearchParams(props.search);
+  const isPreviewing = params.has('builder.preview');
+
   return (
     <Layout>
-      <BuilderComponent model="page" content={content?.data} />
+      {!content.data && !isPreviewing ? (
+        <NotFound />
+      ) : (
+        <BuilderComponent model={MODEL_NAME} content={content?.data} />
+      )}
     </Layout>
   );
 }
