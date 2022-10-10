d9b80767eaecf3cdccb00957378e10409de7052f
Ryan McDonnell <ryanmcdonnell@users.noreply.github.com>
2022-10-10 09:30:52 -0700

    Update BuilderComponent.client.tsx

--- src/components/BuilderComponent.client.tsx
+++ src/components/BuilderComponent.client.tsx
@@ -1,6 +1,6 @@
 import {builder, BuilderComponent} from '@builder.io/react';
 import "@builder.io/widgets";
 
-builder.init('cda38653c81344cf8859bd15e4d8e30d');
+builder.init('0282d29654b3443eb24f2a0d4a9a1bb5'); // PoC
 
 export { BuilderComponent };
