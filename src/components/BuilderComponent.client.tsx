d4657319f5dca5c60445e8ff57674b196d41fa59
Korey Kassir <korey@builder.io>
2023-02-14 15:32:11 -0800

    Update BuilderComponent.client.tsx

--- src/components/BuilderComponent.client.tsx
+++ src/components/BuilderComponent.client.tsx
@@ -1,6 +1,7 @@
 import {builder, BuilderComponent} from '@builder.io/react';
 import "@builder.io/widgets";
 
+// API key for your Builder space: https://www.builder.io/c/docs/using-your-api-key#finding-your-public-api-key
 builder.init('cda38653c81344cf8859bd15e4d8e30d');
 
 export { BuilderComponent };
