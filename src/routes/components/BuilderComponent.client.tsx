4eb741a7dfef2175f72dfa2b8b3149f4bafbd390
Korey Kassir <korey@builder.io>
2023-02-14 15:31:55 -0800

    Update BuilderComponent.client.tsx

--- src/routes/components/BuilderComponent.client.tsx
+++ src/routes/components/BuilderComponent.client.tsx
@@ -1,6 +1,7 @@
 // Do stuff here
 import {BuilderComponent, builder, Builder} from '@builder.io/react';
 
+// API key for your Builder space: https://www.builder.io/c/docs/using-your-api-key#finding-your-public-api-key
 builder.init('cda38653c81344cf8859bd15e4d8e30d');
 
 // Add custom components
